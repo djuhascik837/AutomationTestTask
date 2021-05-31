5345c218eaec283f253aa1641b0e0eeb860eaccf
djuhascik837 <djuhascik837@users.noreply.github.com>
2021-05-31 13:11:29 +0100

    TestCase 1,2 completion

--- cypress/integration/Tests/TestCases.js
+++ cypress/integration/Tests/TestCases.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-context('Test Case 1', () => {
+context('Test Case 1 and 2', () => {
   beforeEach(() => {
 
     cy.intercept({
@@ -11,16 +11,44 @@ context('Test Case 1', () => {
     cy.visit('https://the-internet.herokuapp.com/challenging_dom');
   })
 
-  it('Click Edit hyperlink', () => { 
+  it('Click Edit and delete hyperlinks', () => { 
       
     cy.wait("@mainPage").then(({response}) => { //This waits for the mainPage call before moving onto the next step, ensuring site is loaded.
       expect(response.statusCode).to.equal(200);
-      cy.get('[href="#edit"]').first().click(); //click first edit button
-      
-      //This checks that the URL has changed to #edit
-      cy.on("url:changed", (newUrl) => {
-        expect(newUrl).to.contain("#edit")
-      })
+
+      //this clicks the refrenced button and checks URL has changed
+      cy.clickBtn(":nth-child(1) > :nth-child(7) > [href='#edit']", "#edit");
+      cy.clickBtn(":nth-child(1) > :nth-child(7) > [href='#delete']", "#delete");
+
+      cy.clickBtn(":nth-child(2) > :nth-child(7) > [href='#edit']", "#edit");
+      cy.clickBtn(":nth-child(2) > :nth-child(7) > [href='#delete']", "#delete");
+
+      cy.clickBtn(":nth-child(3) > :nth-child(7) > [href='#edit']", "#edit");
+      cy.clickBtn(":nth-child(3) > :nth-child(7) > [href='#delete']", "#delete");
+
+      cy.clickBtn(":nth-child(4) > :nth-child(7) > [href='#edit']", "#edit");
+      cy.clickBtn(":nth-child(4) > :nth-child(7) > [href='#delete']", "#delete");
+
+      cy.clickBtn(":nth-child(5) > :nth-child(7) > [href='#edit']", "#edit");
+      cy.clickBtn(":nth-child(5) > :nth-child(7) > [href='#delete']", "#delete");
+
+      cy.clickBtn(":nth-child(6) > :nth-child(7) > [href='#edit']", "#edit");
+      cy.clickBtn(":nth-child(6) > :nth-child(7) > [href='#delete']", "#delete");
+
+      cy.clickBtn(":nth-child(7) > :nth-child(7) > [href='#edit']", "#edit");
+      cy.clickBtn(":nth-child(7) > :nth-child(7) > [href='#delete']", "#delete");
+
+      cy.clickBtn(":nth-child(8) > :nth-child(7) > [href='#edit']", "#edit");
+      cy.clickBtn(":nth-child(8) > :nth-child(7) > [href='#delete']", "#delete");
+
+      cy.clickBtn(":nth-child(9) > :nth-child(7) > [href='#edit']", "#edit");
+      cy.clickBtn(":nth-child(9) > :nth-child(7) > [href='#delete']", "#delete");
+
+      cy.clickBtn(":nth-child(9) > :nth-child(7) > [href='#edit']", "#edit");
+      cy.clickBtn(":nth-child(9) > :nth-child(7) > [href='#delete']", "#delete");
+
+      cy.clickBtn(":nth-child(10) > :nth-child(7) > [href='#edit']", "#edit");
+      cy.clickBtn(":nth-child(10) > :nth-child(7) > [href='#delete']", "#delete");
 
     })
   })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('clickBtn', (btnRef, urlRef) => {
+    cy.get(btnRef).click();
+    cy.url().should('include', urlRef);
+})
